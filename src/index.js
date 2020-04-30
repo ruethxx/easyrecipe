ac1c418b5c1257b65d40f186720071e4d0dc5aaa
Melanie-lab <melanie.storr@yahoo.de>
2020-04-30 10:22:25 +0200

    fixes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import navBar from "./js/nav_bar";
 import store from "./js/dataStorage";
 import fillRecipeClones from "./js/dataOutput";
 
-// prettier-ignore
 const showAllPage = "http://localhost:1234/html/show_all_recipes.html";
 const addPage = "http://localhost:1234/html/add_index.html";
 
